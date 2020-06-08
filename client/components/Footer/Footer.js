656a5dc5bfa229c2cee89ce25093ccedaef33a8c
whatwewant <tobewhatwewant@gmail.com>
2020-06-08 19:32:03 +0800

    feat(footer): hide footer

--- client/components/Footer/Footer.js
+++ client/components/Footer/Footer.js
@@ -1,8 +1,8 @@
 import './Footer.scss';
 import React, { PureComponent as Component } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col } from 'antd';
-import { Icon } from 'antd';
+// import { Row, Col } from 'antd';
+// import { Icon } from 'antd';
 
 const version = process.env.version;
 class Footer extends Component {
@@ -13,54 +13,55 @@ class Footer extends Component {
     footList: PropTypes.array
   };
   render() {
-    return (
-      <div className="footer-wrapper">
-        <Row className="footer-container">
-          {this.props.footList.map(function(item, i) {
-            return (
-              <FootItem
-                key={i}
-                linkList={item.linkList}
-                title={item.title}
-                iconType={item.iconType}
-              />
-            );
-          })}
-        </Row>
-      </div>
-    );
+    // return (
+    //   <div className="footer-wrapper">
+    //     <Row className="footer-container">
+    //       {this.props.footList.map(function(item, i) {
+    //         return (
+    //           <FootItem
+    //             key={i}
+    //             linkList={item.linkList}
+    //             title={item.title}
+    //             iconType={item.iconType}
+    //           />
+    //         );
+    //       })}
+    //     </Row>
+    //   </div>
+    // );
+    return <div style={{ display: 'none' }} />;
   }
 }
 
-class FootItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-  static propTypes = {
-    linkList: PropTypes.array,
-    title: PropTypes.string,
-    iconType: PropTypes.string
-  };
-  render() {
-    return (
-      <Col span={6}>
-        <h4 className="title">
-          {this.props.iconType ? <Icon type={this.props.iconType} className="icon" /> : ''}
-          {this.props.title}
-        </h4>
-        {this.props.linkList.map(function(item, i) {
-          return (
-            <p key={i}>
-              <a href={item.itemLink} className="link">
-                {item.itemTitle}
-              </a>
-            </p>
-          );
-        })}
-      </Col>
-    );
-  }
-}
+// class FootItem extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   static propTypes = {
+//     linkList: PropTypes.array,
+//     title: PropTypes.string,
+//     iconType: PropTypes.string
+//   };
+//   render() {
+//     return (
+//       <Col span={6}>
+//         <h4 className="title">
+//           {this.props.iconType ? <Icon type={this.props.iconType} className="icon" /> : ''}
+//           {this.props.title}
+//         </h4>
+//         {this.props.linkList.map(function(item, i) {
+//           return (
+//             <p key={i}>
+//               <a href={item.itemLink} className="link">
+//                 {item.itemTitle}
+//               </a>
+//             </p>
+//           );
+//         })}
+//       </Col>
+//     );
+//   }
+// }
 
 Footer.defaultProps = {
   footList: [
