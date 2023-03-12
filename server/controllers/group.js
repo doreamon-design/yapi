dc8319b606217dbf41acf12ed7cb072733e9dfe3
whatwewant <tobewhatwewant@outlook.com>
2023-03-12 11:34:01 +0800

    chore: disable 个人空间

--- server/controllers/group.js
+++ server/controllers/group.js
@@ -454,7 +454,7 @@ class groupController extends baseController {
       privateGroup = privateGroup.toObject();
       privateGroup.group_name = '个人空间';
       privateGroup.role = 'owner';
-      newResult.unshift(privateGroup);
+      // newResult.unshift(privateGroup);
     }
 
     ctx.body = yapi.commons.resReturn(newResult);
