a242f9c2a3cf5ed352a127411f26b4ca7bd94eea
whatwewant <tobewhatwewant@outlook.com>
2023-03-14 20:34:55 +0800

    fix: disable nav for docs

--- ydocfile.js
+++ ydocfile.js
@@ -8,8 +8,8 @@ module.exports = {
     "common": { // 通用默认配置，包括主页配置等
         "title": "可视化接口管理平台", //page title
         "footer": "&copy; 2018 <a href=\"https://ymfe.org\">YMFE</a> Team. Build by <a href=\"http://ued.qunar.com/ydoc/\">ydoc</a>&nbsp;", // 通用尾
-        "home": '<img src="./images/logo_header@2x.png" /> &nbsp;YAPI', 
-        "homeUrl": "./index.html", 
+        "home": '<img src="./images/logo_header@2x.png" /> &nbsp;YAPI',
+        "homeUrl": "./index.html",
         "keywords": "api管理,接口管理,接口文档,api文档",
         "description": "YApi 是高效、易用、功能强大的 api 管理平台，旨在为开发、产品、测试人员提供更优雅的接口管理服务。可以帮助开发者轻松创建、发布、维护 API，YApi 还为用户提供了优秀的交互体验，开发人员只需利用平台提供的接口数据写入工具以及简单的点击操作就可以实现接口的管理。"
     },
@@ -28,14 +28,14 @@ module.exports = {
                 "button": [{ // 按钮组将显示在banner底部
                     "name": "产品演示",
                     "href": "http://yapi.demo.qunar.com"
-                },{
+                }, {
                     "name": "Github",
                     "href": "https://github.com/YMFE/yapi"
                 }],
                 "intro": [{ // 首页的正文部分，数组项依次渲染，可重复使用
                     "title": "为 API 开发者设计的管理平台", // 介绍板块标题
                     "desc": "YApi 让接口开发更简单高效，让接口的管理更具可读性、可维护性，让团队协作更合理。" // 介绍板块描述
-                },{
+                }, {
                     "title": "",
                     "detail": {
                         "type": "thumbnail", // 三列布局的介绍板块
@@ -43,11 +43,11 @@ module.exports = {
                             "name": "项目管理", // 板块名称
                             "src": "./images/intro_page_1.png", // 缩略图
                             "desc": "YApi 成熟的团队管理<br>扁平化项目权限配置满足各类企业的需求" // 描述
-                        },{
+                        }, {
                             "name": "接口管理",
                             "src": "./images/intro_page_2.png",
                             "desc": "友好的接口文档<br>基于 websocket 的多人协作接口编辑功能和类 postman 测试工具，让多人协作成倍提升开发效率"
-                        },{
+                        }, {
                             "name": "MockServer",
                             "src": "./images/intro_page_3.png",
                             "desc": "基于 Mockjs<br>使用简单功能强大"
@@ -70,168 +70,168 @@ module.exports = {
             },
             "content": "./doc/home.md"
         }, {
-        "name": "usage",
-        "title": "使用手册",
-        "banner": {
-            "title": "YApi",
-            "description": "高效、易用、功能强大的api管理平台，旨在为开发、产品、测试人员提供更优雅的接口管理服务。"
-        },
-        "content": {
-            "sidebar": true,
-            "multi": true,
-            "index": "./doc/page/usage/getfamiliar.md",
-            "pages": [{
-                "name": "认识 YApi",
-                "index": "getfamiliar",
-                "content": "./doc/page/usage/getfamiliar.md"
-            },{
-                "name": "创建第一个API",
-                "index": "quickstart",
-                "content": "./doc/page/usage/quickstart.md"
-            },{
-                "name": "管理分组与项目",
-                "index": "manage",
-                "content": "./doc/page/usage/manage.md"
-            },{
-                "name": "项目操作",
-                "index": "project",
-                "content": "./doc/page/usage/project.md"
-            },{
-                "name": "接口操作",
-                "index": "interface",
-                "content": "./doc/page/usage/api.md"
-            },{
-                "name": "普通 Mock",
-                "index": "mock",
-                "content": "./doc/page/usage/mock.md"
-            },{
-                "name": "高级 Mock",
-                "index": "adv_mock",
-                "content": "./doc/page/usage/adv_mock.md"
-            },{
-                "name": "使用测试集",
-                "index": "case",
-                "content": "./doc/page/usage/case.md"
-            },{
-                "name": "数据导入",
-                "index": "data",
-                "content": "./doc/page/usage/data.md"
+            "name": "usage",
+            "title": "使用手册",
+            "banner": {
+                "title": "YApi",
+                "description": "高效、易用、功能强大的api管理平台，旨在为开发、产品、测试人员提供更优雅的接口管理服务。"
+            },
+            "content": {
+                "sidebar": true,
+                "multi": true,
+                "index": "./doc/page/usage/getfamiliar.md",
+                "pages": [{
+                    "name": "认识 YApi",
+                    "index": "getfamiliar",
+                    "content": "./doc/page/usage/getfamiliar.md"
+                }, {
+                    "name": "创建第一个API",
+                    "index": "quickstart",
+                    "content": "./doc/page/usage/quickstart.md"
+                }, {
+                    "name": "管理分组与项目",
+                    "index": "manage",
+                    "content": "./doc/page/usage/manage.md"
+                }, {
+                    "name": "项目操作",
+                    "index": "project",
+                    "content": "./doc/page/usage/project.md"
+                }, {
+                    "name": "接口操作",
+                    "index": "interface",
+                    "content": "./doc/page/usage/api.md"
+                }, {
+                    "name": "普通 Mock",
+                    "index": "mock",
+                    "content": "./doc/page/usage/mock.md"
+                }, {
+                    "name": "高级 Mock",
+                    "index": "adv_mock",
+                    "content": "./doc/page/usage/adv_mock.md"
+                }, {
+                    "name": "使用测试集",
+                    "index": "case",
+                    "content": "./doc/page/usage/case.md"
+                }, {
+                    "name": "数据导入",
+                    "index": "data",
+                    "content": "./doc/page/usage/data.md"
+                }
+                ]
             }
-        ]
-        }
-    },
-//   {
-//       "name": "manage",
-//       "title": "管理学院",
-//       "banner": {
-//           "title": "管理学院",
-//           "description": "超级管理员、分组/项目组长的学院。"
-//       },
-//       "content": {
-//           "sidebar": true,
-//           "multi": true,
-//           "index": "./doc/page/manage/intro.md",
-//           "pages": [{
-//               "name": "介绍",
-//               "content": "./doc/page/manage/intro.md"
-//           }, {
-//               "name": "部署",
-//               "content": "./doc/page/manage/build.md"
-//           }, {
-//               "name": "超管职责",
-//               "content": "./doc/page/manage/admin.md"
-//           }, {
-//               "name": "版本升级",
-//               "content": "./doc/page/manage/update.md"
-//           }]
-//       }
-//   },
-    {
-    "name": "devops",
-    "title": "内网部署",
-    "banner": {
-        "title": "内网部署",
-        "description": "部署 YApi 平台是非常容易的，即便您不懂 nodejs 或者 mongodb"
-    },
-    "compile": "markdown",
-    "menuLevel": 2,
-    "content": "./doc/page/manage/build.md"
-  },
-    {
-      "name": "plugin",
-      "title": "插件 Wiki",
-      "banner": {
-          "title": "插件",
-          "description": "可根据业务需求，定制化功能"
-      },
-      "content": {
-          "sidebar": true,
-          "multi": true,
-          "index": "./doc/page/plugin/redev.md",
-          "pages": [{
-            "name": "二次开发",
-            "index": "redev",
-            "content": "./doc/page/plugin/redev.md"
-        },{
-              "name": "插件管理",
-              "index": "plugin-index",
-              "content": "./doc/page/plugin/index.md"
-          },{
-            "name": "插件开发",
-            "index": "plugin-dev",
-            "content": "./doc/page/plugin/dev.md"
-        },{
-            "name": "钩子列表",
-            "index": "plugin-hooks",
-            "content": "./doc/page/plugin/hooks.md"
-        },{
-            "name": "插件列表",
-            "index": "plugin-list",
-            "content": "./doc/page/plugin/list.md"
-        }
-        ]
-      }
-  },
-  {
-    "name": "qa",
-    "title": "常见问题",
-    "banner": {
-        "title": "常见问题",
-        "description": "这里列举了常见的问题"
-    },
-    "compile": "markdown",
-    "menuLevel": 2,
-    "content": "./doc/page/usage/qa.md"
-  },
-  {
-    "name": "releases",
-    "title": "版本记录",
-    "banner": {
-        "title": "YApi",
-        "description": "这里列举了 YApi 的历史版本、发布时间及变更记录，帮助你追溯到每个版本的演进过程。"
-    },
-    "compile": "markdown",
-    "menuLevel": 2,
-    "content": "./CHANGELOG.md"
-  },{
-    "name": "产品演示",
-    "title": "产品演示",
-    "url": "http://yapi.demo.qunar.com/"
-  },
-    {
-        "name": "api",
-        "title": "",
-        "banner": {
-            "title": "api",
-            "description": "api文档"
         },
-        "content": "./server/controllers/+(follow|base|group|interface|interfaceCol|log|project|user).js",
-        "options": {
-            "type": "interface",
-            "source": true,
-            "categories":["group","user","project", "interface","follow"]
-        }
-    }],
+        //   {
+        //       "name": "manage",
+        //       "title": "管理学院",
+        //       "banner": {
+        //           "title": "管理学院",
+        //           "description": "超级管理员、分组/项目组长的学院。"
+        //       },
+        //       "content": {
+        //           "sidebar": true,
+        //           "multi": true,
+        //           "index": "./doc/page/manage/intro.md",
+        //           "pages": [{
+        //               "name": "介绍",
+        //               "content": "./doc/page/manage/intro.md"
+        //           }, {
+        //               "name": "部署",
+        //               "content": "./doc/page/manage/build.md"
+        //           }, {
+        //               "name": "超管职责",
+        //               "content": "./doc/page/manage/admin.md"
+        //           }, {
+        //               "name": "版本升级",
+        //               "content": "./doc/page/manage/update.md"
+        //           }]
+        //       }
+        //   },
+        {
+            "name": "devops",
+            "title": "内网部署",
+            "banner": {
+                "title": "内网部署",
+                "description": "部署 YApi 平台是非常容易的，即便您不懂 nodejs 或者 mongodb"
+            },
+            "compile": "markdown",
+            "menuLevel": 2,
+            "content": "./doc/page/manage/build.md"
+        },
+        {
+            "name": "plugin",
+            "title": "插件 Wiki",
+            "banner": {
+                "title": "插件",
+                "description": "可根据业务需求，定制化功能"
+            },
+            "content": {
+                "sidebar": true,
+                "multi": true,
+                "index": "./doc/page/plugin/redev.md",
+                "pages": [{
+                    "name": "二次开发",
+                    "index": "redev",
+                    "content": "./doc/page/plugin/redev.md"
+                }, {
+                    "name": "插件管理",
+                    "index": "plugin-index",
+                    "content": "./doc/page/plugin/index.md"
+                }, {
+                    "name": "插件开发",
+                    "index": "plugin-dev",
+                    "content": "./doc/page/plugin/dev.md"
+                }, {
+                    "name": "钩子列表",
+                    "index": "plugin-hooks",
+                    "content": "./doc/page/plugin/hooks.md"
+                }, {
+                    "name": "插件列表",
+                    "index": "plugin-list",
+                    "content": "./doc/page/plugin/list.md"
+                }
+                ]
+            }
+        },
+        {
+            "name": "qa",
+            "title": "常见问题",
+            "banner": {
+                "title": "常见问题",
+                "description": "这里列举了常见的问题"
+            },
+            "compile": "markdown",
+            "menuLevel": 2,
+            "content": "./doc/page/usage/qa.md"
+        },
+        {
+            "name": "releases",
+            "title": "版本记录",
+            "banner": {
+                "title": "YApi",
+                "description": "这里列举了 YApi 的历史版本、发布时间及变更记录，帮助你追溯到每个版本的演进过程。"
+            },
+            "compile": "markdown",
+            "menuLevel": 2,
+            "content": "./CHANGELOG.md"
+        }, {
+            "name": "产品演示",
+            "title": "产品演示",
+            "url": "http://yapi.demo.qunar.com/"
+        },
+        {
+            "name": "api",
+            "title": "",
+            "banner": {
+                "title": "api",
+                "description": "api文档"
+            },
+            "content": "./server/controllers/+(follow|base|group|interface|interfaceCol|log|project|user).js",
+            "options": {
+                "type": "interface",
+                "source": true,
+                "categories": ["group", "user", "project", "interface", "follow"]
+            }
+        }],
     "resources": {
         "images": "./doc/images",
         "styles": "./doc/styles"
