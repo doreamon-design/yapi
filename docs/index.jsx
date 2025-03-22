9278b45c5907425bb9a489b87ab93b854f3fd5ea
Zero <tobewhatwewant@outlook.com>
2025-03-22 09:15:11 +0800

    chore: update doc

--- docs/index.jsx
+++ docs/index.jsx
@@ -4,7 +4,6 @@ banner:
   desc: '旨在为开发、产品、测试人员提供更优雅的接口管理服务。可以帮助开发者轻松创建、发布、维护 API'
   btns: 
     - { name: '开始', href: './documents/index.html', primary: true }
-    // - { name: 'Github >', href: 'https://github.com/ymfe/yapi' }
 features: 
   - { name: '权限管理', desc: 'API-DOCS 成熟的团队管理扁平化项目权限配置满足各类企业的需求' }
   - { name: '可视化接口管理', desc: '基于 websocket 的多人协作接口编辑功能和类 postman 测试工具，让多人协作成倍提升开发效率' }
@@ -13,17 +12,15 @@ features:
   - { name: '数据导入', desc: '支持导入 swagger, postman, har 数据格式，方便迁移旧项目'}
   - { name: '插件机制', desc: '强大的插件机制，满足各类业务需求'}
 
-// footer:
-//   copyRight:
-//     name: 'YMFE Team'
-//     href: 'https://ymfe.org/'
-//   links:
-//     团队网址:
-//       - { name: 'YMFE', href: 'https://ymfe.org/' }
-//       - { name: 'YMFE Blog', href: 'https://blog.ymfe.org/' }
-//     Git仓库:
-//       - { name: 'Github', href: 'https://github.com/YMFE/yapi' }
-//       - { name: 'Github Issue', href: 'https://github.com/YMFE/yapi/issues' }
+footer:
+  copyRight:
+    name: 'APIDOCS Team'
+    href: 'https://apidocs.org/'
+  links:
+    团队网址:
+      - { name: 'APIDOCS', href: 'https://apidocs.org/' }
+    Git仓库:
+    - { name: 'Github', href: 'https://github.com/APIDOCS/apidocs' }
 
 ---
 {
